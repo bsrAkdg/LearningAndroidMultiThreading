67c6e11d445be851d3973327d382b309575f6d9e
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-07-20 00:29:03 +0300

    Thread synchronization

--- MultiThreadingSamples/app/src/main/java/com/bsrakdg/multithreadingsamples/demonstrations/visibility/VisibilityDemonstration.java
+++ MultiThreadingSamples/app/src/main/java/com/bsrakdg/multithreadingsamples/demonstrations/visibility/VisibilityDemonstration.java
@@ -2,8 +2,11 @@ package com.bsrakdg.multithreadingsamples.demonstrations.visibility;
 
 public class VisibilityDemonstration {
 
-    // private static int sCount = 0; STEP 1
+    // private static int sCount = 0; // STEP 1
     private volatile static int sCount = 0; // STEP 2
+
+    // Thread synchronized
+    private static final Object LOCK = new Object();
 
     public static void main(String[] args) {
         new Consumer().start();
@@ -20,13 +23,16 @@ public class VisibilityDemonstration {
         public void run() {
             int localValue = -1;
             while (true) {
-                // Consumer doesn't handle sCount changes on Producer, this is a problem
-                if (localValue != sCount) {
-                    System.out.println("Consumer: detected count change " + sCount);
-                    localValue = sCount;
-                }
-                if (sCount >= 5) {
-                    break;
+                // synchronized : guaranteed that it will hold the lock until this statement completes
+                synchronized (LOCK) {
+                    // Consumer doesn't handle sCount changes on Producer, this is a problem
+                    if (localValue != sCount) {
+                        System.out.println("Consumer: detected count change " + sCount);
+                        localValue = sCount;
+                    }
+                    if (sCount >= 5) {
+                        break;
+                    }
                 }
             }
             System.out.println("Consumer: terminating");
@@ -36,15 +42,16 @@ public class VisibilityDemonstration {
     static class Producer extends Thread {
         @Override
         public void run() {
-            while (sCount < 5) {
-                int localValue = sCount;
-                localValue++;
-                System.out.println("Producer: incrementing count to " + localValue);
-                sCount = localValue;
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    return;
+            while (true) {
+                synchronized (LOCK) {
+                    int localValue = sCount;
+                    localValue++;
+                    System.out.println("Producer: incrementing count to " + localValue);
+                    sCount = localValue;
+
+                    if (sCount >= 5) {
+                        break;
+                    }
                 }
             }
             System.out.println("Producer: terminating");
